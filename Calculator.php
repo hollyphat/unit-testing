2e4257e1b0129137cdf0d0fa1eb79d71e3ac6bed
Olatunji <sirhollyfat@gmail.com>
2019-11-25 14:53:15 +0100

    Create class calculator

---
+++ Calculator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hollyphat
+ * Date: 11/25/2019
+ * Time: 2:52 PM
+ */
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
